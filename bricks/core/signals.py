81340c1f972af6ea73e448e8942a729afcbd9608
kem wan <kem.wan@sandalwoodadvisors.com>
2023-11-15 15:05:50 +0800

    add signals

---
+++ bricks/core/signals.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023-11-15 14:04
+# @Author  : Kem
+# @Desc    :
+class Signal(Exception):
+    def __init__(self, **kwargs):
+        self.form = self.__class__
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+# 中断信号, 中断后续流程
+class Break(Signal):
+    ...
+
+
+# 退出信号, 退出当前任务
+class Exit(Signal):
+    ...
