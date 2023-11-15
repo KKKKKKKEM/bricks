8a0f4295604420e423f8e82aed1e6595544f1ae1
kem wan <kem.wan@sandalwoodadvisors.com>
2023-11-15 12:39:20 +0800

    the log format is optimized

--- bricks/__init__.py
+++ bricks/__init__.py
@@ -2,7 +2,18 @@
 # @Time    : 2023-11-13 17:45
 # @Author  : Kem
 # @Desc    :
+import sys
+
+from loguru import logger
 
 from bricks.core.dispatch import Dispatcher, Task  # noqa
 from bricks.lib.request import Request
 from bricks.lib.response import Response
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level}</level>  | <level>{message}</level>",
+    colorize=True,
+
+)
