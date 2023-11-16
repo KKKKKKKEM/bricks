17c42f3a19254ae1b48cf87d682fb9ced61bb5ae
kem wan <kem.wan@sandalwoodadvisors.com>
2023-11-16 23:27:02 +0800

    1. 补充事件监控 2. 添加事件注册 3. 优化任务提交

--- bricks/core/genesis.py
+++ bricks/core/genesis.py
@@ -8,7 +8,8 @@ import inspect
 from loguru import logger
 
 from bricks import const
-from bricks.core import signals, events, dispatch
+from bricks.core import signals, dispatch
+from bricks.core.events import Event
 from bricks.lib.context import Flow, Context
 
 
@@ -70,18 +71,21 @@ class Chaos(metaclass=MetaClass):
         def wrapper(*args, **kwargs):
             try:
                 self.before_start()
-            except signals.Signal as sig:
-                if sig.form == signals.Break:
-                    logger.debug(
-                        f'[收到信号] 信号类型: 中断信号; 任务: {self.__class__.__name__}; 信号来源: 启动之前; 信号影响: 中断后续流程')
-                    return
+
+            except signals.Break:
+                logger.debug(f'[{const.BEFORE_START}] 任务被中断')
+                return
+
+            except signals.Signal as e:
+                logger.warning(f"[{const.BEFORE_START}] 无法处理的信号类型: {e}")
 
             ret = raw_method(*args, **kwargs)
 
             try:
                 self.before_close()
-            except signals.Signal:
-                pass
+            except signals.Signal as e:
+                logger.warning(f"[{const.BEFORE_START}] 无法处理的信号类型: {e}")
+
             return ret
 
         return wrapper
@@ -96,14 +100,15 @@ class Chaos(metaclass=MetaClass):
 
         @functools.wraps(raw_method)
         def wrapper(*args, **kwargs):
-            events.Event.invoke(
-                Context(
-                    form=const.BEFORE_START,
-                    target=self,
-                    args=args,
-                    kwargs=kwargs
-                )
+            context = Context(
+                form=const.BEFORE_START,
+                target=self,
+                args=args,
+                kwargs=kwargs
             )
+
+            Event.invoke(context)
+
             ret = raw_method(*args, **kwargs)
             return ret
 
@@ -119,14 +124,13 @@ class Chaos(metaclass=MetaClass):
 
         @functools.wraps(raw_method)
         def wrapper(*args, **kwargs):
-            events.Event.invoke(
-                Context(
-                    form=const.BEFORE_CLOSE,
-                    target=self,
-                    args=args,
-                    kwargs=kwargs
-                )
+            context = Context(
+                form=const.BEFORE_CLOSE,
+                target=self,
+                args=args,
+                kwargs=kwargs
             )
+            Event.invoke(context)
             ret = raw_method(*args, **kwargs)
             return ret
 
@@ -142,12 +146,14 @@ class Pangu(Chaos):
             self.dispatcher = dispatch.Dispatcher(max_workers=self.get_attr("concurrency", 1))
 
     def on_consume(self, context: Flow):  # noqa
+        context.next.root == self.on_consume and context.flow()
 
         while True:
             stuff = context.produce()
             if stuff is None: return
 
             while stuff.next and callable(stuff.next):
+                # print(stuff.next)
                 prev = stuff.next
                 if not inspect.iscoroutinefunction(stuff.next):
                     product = [stuff.next(stuff)]
@@ -155,21 +161,21 @@ class Pangu(Chaos):
                     product = stuff.next(stuff)
 
                 for _ in product:
-                    pass
+                    print(stuff.next, _)
 
                 if prev == stuff.next and prev.root not in stuff.flows:
                     stuff.flow({"next": None})
                     break
 
-    def use(self, context: Flow, timeout=None):
+    def submit(self, task: dispatch.Task, timeout=None) -> dispatch.Task:
+        return self.dispatcher.submit_task(task=task, timeout=timeout)
 
-        return self.dispatcher.submit_task(
-            task=dispatch.Task(
-                func=self.on_consume,
-                args=[context]
-            ),
-            timeout=timeout
-        )
+    def active(self, task: dispatch.Task, timeout=-1) -> dispatch.Task:
+        return self.dispatcher.active_task(task=task, timeout=timeout)
+
+    def use(self, form, *events):
+        context = Context(form=form, target=self)
+        Event.register(context, *events)
 
 
 if __name__ == '__main__':
