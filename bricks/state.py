77c36a006aac6c6da0dd7bc2a95880eb6aae20ed
kem wan <kem.wan@sandalwoodadvisors.com>
2023-12-05 20:57:15 +0800

    添加 template 爬虫基类, 相对于 form 爬虫, template 是流程固化的配置式爬虫,但是 form 可以进行流程自定义

--- bricks/state.py
+++ bricks/state.py
@@ -19,7 +19,7 @@ __all__ = (
 MACHINE_ID = hashlib.sha256(uuid.UUID(int=uuid.getnode()).hex[-12:].encode()).hexdigest()
 
 # 当前框架版本
-VERSION = "0.0.6.9"
+VERSION = "0.0.6.10"
 
 # 全局变量
 G = variable.VariableG()
