cc730f84f449775f0b0ab36568f67a92dd649b50
kem wan <kem.wan@sandalwoodadvisors.com>
2023-12-10 13:24:04 +0800

    添加 by_sqllite

--- bricks/plugins/make_seeds.py
+++ bricks/plugins/make_seeds.py
@@ -10,6 +10,7 @@ import time
 from typing import Optional, Union, List
 from urllib.parse import urlencode
 
+from bricks.db.sqllite import SqlLite
 from bricks.utils.csv_ import Reader
 
 LIMIT_PATTERN = re.compile(r"(LIMIT\s+)(\d+)", flags=re.IGNORECASE)
@@ -121,6 +122,62 @@ def by_mongo(
         skip = raw_skip
 
     for rows in conn.iter_data(collection=path, query=query, skip=skip, count=batch_size, database=database, sort=sort):
+        skip += len(rows)
+        record.update({record_key: skip})
+        yield rows
+
+
+def by_sqllite(
+        path: str,
+        conn: SqlLite,
+        batch_size: int = 10000,
+        skip: Union[str, int] = ...,
+        record: Optional[dict] = None,
+):
+    """
+    通过 sqllite 初始化
+
+    :param path: 查询 sql
+    :param conn: SqlLite 连接
+    :param batch_size: 一次获取多少条
+    :param skip: 要跳过多少条
+    :param record: 历史记录, 用于断点续投
+    :return:
+    """
+    record = record or {}
+    if skip is ...:
+        if record:
+            skip = 'auto'
+        else:
+            skip = 0
+
+    raw_skip = skip
+    record_key = _make_key({
+        "path": path,
+        "skip": raw_skip,
+    })
+    if raw_skip == 'auto':
+        skip = int(record.get(record_key, 0))
+    else:
+        skip = raw_skip
+    if skip != 0:
+        # 原来就有 offset
+        if OFFSET_PATTERN.search(path):
+            def add_skip(match):
+                # 将捕获的数字转换为整数，加上 skip，然后格式化回字符串
+                return f"{match.group(1)}{int(match.group(2)) + skip}"
+
+            path = OFFSET_PATTERN.sub(add_skip, path)
+
+        # 没有 offset 但是有 limit
+        elif not LIMIT_PATTERN.search(path):
+            path = path + f"LIMIT -1 OFFSET {skip}"
+
+        # offset 和 limit 都没有
+        else:
+            path = LIMIT_PATTERN.sub(r"\1\2 OFFSET " + str(skip), path)
+
+    for rows in conn.find(sql=path, batch_size=batch_size):
         skip += len(rows)
         record.update({record_key: skip})
         yield rows
