de82f6dcb57028be45d9ed2b09c5beface3e83ec
kem wan <kem.wan@sandalwoodadvisors.com>
2023-12-12 16:06:58 +0800

    优化 Header

--- bricks/lib/headers.py
+++ bricks/lib/headers.py
@@ -2,84 +2,50 @@
 # @Time    : 2023-11-14 20:44
 # @Author  : Kem
 # @Desc    : Header Model
-from typing import Callable, Mapping
+from collections import UserDict
+from typing import Mapping
 
 
-class Header(dict):
+class Header(UserDict):
 
-    def __init__(self, data=None, _dtype: Callable = None, _ktype: Callable = None, **kwargs):
-        self.dtype = _dtype or str
-        self.ktype = _ktype or str
-        self.comps = [
-            lambda x: str(x).lower(),
-            lambda x: str(x).upper(),
-            lambda x: str(x).title(),
-        ]
+    def __init__(self, data=None, **kwargs):
+        self.caches = {}
         if data is None:
             data = {}
         super().__init__({**data, **kwargs})
 
     def __setitem__(self, key, value):
-        key: str = self.ktype(key)
-        keys = self.keys()
-
-        for comp in self.comps:
-            nkey = comp(key)
-            if nkey in keys:
-                return super().__setitem__(nkey, self.dtype(value))
-
+        if key.lower() in self.caches:
+            return super().__setitem__(self.caches[key.lower()], value)
         else:
-            return super().__setitem__(key, self.dtype(value))
+            self.caches[key.lower()] = key
+            return super().__setitem__(key, value)
 
     def __getitem__(self, key):
 
-        key: str = self.ktype(key)
-        keys = self.keys()
-
-        for comp in self.comps:
-            nkey = comp(key)
-            if nkey in keys:
-                return super().__getitem__(nkey)
+        if key.lower() in self.caches:
+            return super().__getitem__(self.caches[key.lower()])
         else:
             return super().__getitem__(key)
 
     def __delitem__(self, key):
-        key: str = self.ktype(key)
-        keys = self.keys()
-
-        for comp in self.comps:
-            nkey = comp(key)
-            if nkey in keys:
-                return super().__delitem__(nkey)
+        if key.lower() in self.caches:
+            return super().__delitem__(self.caches[key.lower()])
         else:
             return super().__delitem__(key)
 
     def __eq__(self, other):
         if isinstance(other, (Mapping, dict)):
-            other = self.__class__(other, dtype=self.dtype, ktype=self.ktype)
+            other = self.__class__(other)
         else:
             return NotImplemented
         # Compare insensitively
         return self.items() == other.items()
 
     def __contains__(self, __o: object) -> bool:
-        key: str = self.ktype(__o)
-        keys = self.keys()
-
-        for comp in self.comps:
-            nkey = comp(key)
-            if nkey in keys:
-                return True
+        if isinstance(__o, str):
+            key = __o.lower()
         else:
-            return False
+            key = __o
 
-    def setdefault(self, __key, __default):  # noqa
-        key: str = self.ktype(__key)
-        keys = self.keys()
-
-        for comp in self.comps:
-            nkey = comp(key)
-            if nkey in keys:
-                return super().setdefault(nkey, self.dtype(__default))
-        else:
-            return super().setdefault(key, self.dtype(__default))
+        return key in self.caches
