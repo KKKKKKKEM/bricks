8c07469b19bdceb1f8aabda29aa79ca981ab42e0
kem wan <kem.wan@sandalwoodadvisors.com>
2023-12-17 16:30:24 +0800

    出现 Switch 信号时直接 continue

--- bricks/core/genesis.py
+++ bricks/core/genesis.py
@@ -218,7 +218,7 @@ class Pangu(Chaos):
                         return
 
                     except signals.Switch:
-                        pass
+                        continue
 
                     except signals.Retry:
                         context.retry()
