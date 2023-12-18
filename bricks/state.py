b41c02a3073b6cd481d95eeecb22a50bf24f9acd
kem wan <kem.wan@sandalwoodadvisors.com>
2023-12-18 14:08:01 +0800

    版本号更新

--- bricks/state.py
+++ bricks/state.py
@@ -19,7 +19,7 @@ __all__ = (
 MACHINE_ID = hashlib.sha256(uuid.UUID(int=uuid.getnode()).hex[-12:].encode()).hexdigest()
 
 # 当前框架版本
-VERSION = "0.0.12.2"
+VERSION = "0.0.12.3"
 
 # 全局变量
 G = variable.VariableG()
