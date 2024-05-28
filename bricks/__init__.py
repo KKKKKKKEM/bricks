703fac82872299bef31ab1a5c00b7fb1a96adf67
kem wan <kem.wan@sandalwoodadvisors.com>
2024-05-28 13:26:54 +0800

    优化日志输出格式, 加入文件名, 固定日志等级宽度

--- bricks/__init__.py
+++ bricks/__init__.py
@@ -6,14 +6,16 @@ import sys
 
 from loguru import logger
 
+from bricks.lib.items import Items
 from bricks.lib.request import Request
 from bricks.lib.response import Response
-from bricks.lib.items import Items
 from bricks.state import *
 
-logger.remove()
+logger.remove(0)
 logger.add(
     sys.stdout,
     format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <7}</level> | <cyan>{file}:{line}</cyan> - <level>{message}</level>",
     colorize=True,
+    backtrace=True,  # 异常时打印回溯信息
+    diagnose=True,  # 更详细的诊断信息
 )
