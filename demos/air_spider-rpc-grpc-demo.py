fee48d00bed11c7cd3458f47e4948da241c5d2bb
Kem <531144129@qq.com>
2025-08-04 21:49:34 +0800

    feat(client): 添加 gRPC 代码生成和示例
    - 新增 gRPC 代码生成脚本和相关文件
    - 添加 SpiderService 定义和消息类型
    - 实现 gRPC 客户端和服务器类
    - 提供 gRPC 爬虫示例

--- demos/air_spider-rpc-grpc-demo.py
+++ demos/air_spider-rpc-grpc-demo.py
@@ -1,14 +1,8 @@
-import threading
-import time
-
 from loguru import logger
 
 from bricks import Request, const
 from bricks.core import events, signals
-from bricks.db.redis_ import Redis
-from bricks.lib.queues import RedisQueue
 from bricks.spider import air
-from bricks.spider.addon import Rpc
 from bricks.spider.air import Context
 
 
@@ -97,92 +91,7 @@ class MySpider(air.Spider):
         # raise signals.Retry
 
 
-def demo1():
-    def on_finish(ctx: MySpider.Context, err: Exception):
-        # 当种子被消耗完成, 就会回调这个方法
-        # 用法:
-        # 1. 你可以将你的结果写入到数据库
-        # 2. 通过 url 回调回传结果, 是用于 golang 搭建的 rpc 服务, 因为 python 的性能实在是太差了, 无法支撑高并发, 所以才有了这个方法
-        if err:
-            logger.debug(f"发生错误 -> {err}")
-        else:
-            logger.debug(f"跑完了 -> {ctx.response.text}")
-
-    def submit_task():
-        while True:
-            for _ in range(10):
-                # 提交任务, 如果服务是 golang 这类跨语言的, 可以使用 redis 队列, 然后那边只用往 redis 里面塞种子就可以了
-                listener.submit({"page": 1})
-
-            logger.debug("提交 10 个种子")
-            time.sleep(2)
-
-    # 这里配置爬虫属性, 就 2 个线程
-    listener = Rpc.listen(MySpider, attrs={"concurrency": 2, "queue_name": "test"}, on_finish=on_finish)
-    # 这边模拟一个后台线程发布任务
-    threading.Thread(target=submit_task).start()
-
-    # 运行 listener 服务
-    listener.run()
-
-
-def demo2():
-    def on_finish(ctx: MySpider.Context, err: Exception):
-        # 当种子被消耗完成, 就会回调这个方法
-        # 用法:
-        # 你可以将你的结果写入到数据库
-
-        if err:
-            logger.debug(f"发生错误 -> {err}")
-            # 把结果写到了 Redis 队列里了
-            redis.add(result_queue_name, {"seeds": ctx.seeds, "error": str(err)})
-        else:
-            logger.debug(f"跑完了 -> {ctx.response.text}")
-            # 把结果写到了 Redis 队列里了
-            redis.add(result_queue_name, {"seeds": ctx.seeds, "data": ctx.response.text})
-
-    redis = Redis(password="0boNLgeuiPIxv7")
-    # 这里创建一个 Redis 队列
-    task_queue = RedisQueue.from_redis(redis)
-    seeds_queue_name = "task-to-submit"
-    result_queue_name = "task-result"
-    # 这里配置爬虫属性, 就 2 个线程
-    listener = Rpc.listen(MySpider, attrs={"concurrency": 2, "queue_name": seeds_queue_name, "task_queue": task_queue},
-                          on_finish=on_finish)
-
-    # 运行 listener 服务, 需要跑任务的时候, 往 Redis 里面放一个种子就可以了
-    listener.run()
-
-
-def demo3():
-    def on_finish(ctx: MySpider.Context, err: Exception):
-        # 当种子被消耗完成, 就会回调这个方法
-        # 用法:
-        # 你可以将你的结果通过回调 url 传回去, 例如你在种子里写一个 callback_url, 然后这里就可以通过 requests 回传结果了
-        # 这种方式适用于使用 golang 开发 api 服务 / 使用方开了 api 服务(使用方通过你的提交接口发布任务, 然后通过这边调用他的回调接口回传结果)
-        callback_uri = ctx.seeds.get("$callbackUri")
-        if callback_uri:
-            if err:
-                logger.debug(f"发生错误 -> {err}")
-                listener.spider.downloader.fetch(
-                    {"url": callback_uri, "method": "POST", "body": {"seeds": ctx.seeds, "error": str(err)}})
-            else:
-                logger.debug(f"跑完了 -> {ctx.response.text}")
-                listener.spider.downloader.fetch(
-                    {"url": callback_uri, "method": "POST", "body": {"seeds": ctx.seeds, "data": ctx.response.text}})
-
-    redis = Redis(password="0boNLgeuiPIxv7")
-    # 这里创建一个 Redis 队列
-    task_queue = RedisQueue.from_redis(redis)
-    seeds_queue_name = "task-to-submit"
-    # 这里配置爬虫属性, 就 2 个线程
-    listener = Rpc.listen(MySpider, attrs={"concurrency": 2, "queue_name": seeds_queue_name, "task_queue": task_queue},
-                          on_finish=on_finish)
-
-    # 运行 listener 服务, 需要跑任务的时候, 往 Redis 里面放一个种子就可以了
-    listener.run()
-
-
 if __name__ == "__main__":
     from bricks.client.gprc_.service import GrpcService
+
     GrpcService.serve(MySpider, attrs={"concurrency": 2, "queue_name": "test"})
